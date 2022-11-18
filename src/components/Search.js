bc592767974f74ebfb80c95547750ab64cbbc158
svwiseowl <sv.alua@mail.ru>
2022-11-18 15:10:59 +0600

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+const Search = () => {
+    return (
+        <header>
+            <h2 className="header__title">Іздеңіз. Таңдаңыз. Сатып алыңыз.</h2>
+            <input
+                type="text"
+                className="header__search"
+                placeholder="Кітаптың атың енгізіңіз"
+            />
+        </header>
+    );
+}
+
+export default Search;
